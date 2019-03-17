c93d915118575f7f3fec2ceb8a855b2a5475dcf3
Evgeny Barnaev <e.barnaev@MacBook-Pro-Evgeny.local>
2019-03-17 18:53:04 +0300

    output and test to other folders

--- cgi-bin/test.py
+++
@@ -1,8 +0,0 @@
-import securer_template
-
-#text_to_send = "Чмафки!"
-#email = 'e.barnaev@altatec.ru'
-#smtp_sender.SendMessage(text_to_send, 'e.barnaev@altatec.ru')
-
-param = 'a.sheviakov'
-print (config.employee[param])
